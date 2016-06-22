c26a98eb9077e3fc9b32185e5ee264960ebebc74
indraprabha <indraprabha@outlook.com>
2016-06-22 12:54:08 +0530

    Traffic Data Analysis - Basic site checkin

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,50 @@
 'use strict';
+
+$(document).ready(function (t) {
+    document.getElementById("headerImage").useMap = "#mapLinks";
+    document.getElementById("services").style.pageBreakAfter = "always";
+    document.getElementById("about").style.pageBreakAfter = "always";
+});
+
+function hideAll() {
+    $("#parkingColorImage").css("display", "none");
+    $("#bicycleColorImage").css("display", "none");
+    $("#roadTubeColorImage").css("display", "none");
+    $("#pedestrianColorImage").css("display", "none");
+    $("#otherColorImage").css("display", "none");
+}
+
+$("#parkingArea").hover(
+    function () {
+        hideAll();
+        $("#parkingColorImage").css("display", "block");
+    }
+);
+
+$("#bicycleArea").hover(
+    function () {
+        hideAll();
+        $("#bicycleColorImage").css("display", "block");
+    }
+);
+
+$("#roadTubeArea").hover(
+    function () {
+        hideAll();
+        $("#roadTubeColorImage").css("display", "block");
+    }
+);
+
+$("#pedestrianArea").hover(
+    function () {
+        hideAll();
+        $("#pedestrianColorImage").css("display", "block");
+    }
+);
+
+$("#otherArea").hover(
+    function () {
+        hideAll();
+        $("#otherColorImage").css("display", "block");
+    }
+);
